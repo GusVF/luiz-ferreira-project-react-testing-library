70b0c69309ab418a8ddf674a6b671f118d49c9ed
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-11-22 16:40:12 -0300

    req-2-2killed

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('tests contents of "About" component', () => {
+  test('if there is a "h2", with text "About Pokedex"', () => {
+    renderWithRouter(<About />);
+
+    const h2Heading = screen.getByRole('heading', {
+      name: /about pokédex/i, level: 2 });
+    expect(h2Heading).toBeInTheDocument();
+  });
+  test('if image is on the screen', () => {
+    renderWithRouter(<About />);
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgAbout = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(imgAbout).toBeInTheDocument();
+    expect(imgAbout.src).toBe(src);
+  });
+});
