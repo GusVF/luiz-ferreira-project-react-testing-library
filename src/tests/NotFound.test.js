375aeb39279f387311fd136f2ab985aa15b7f4c7
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-11-22 17:22:10 -0300

    req-4-2killed

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test if "Not Found" page elements render', () => {
+  test('if page elements render', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundEl = screen.getByRole('heading', {
+      name: /page requested not found/i, level: 2 });
+    expect(notFoundEl).toBeInTheDocument();
+  });
+
+  test('if image renders with "url"', () => {
+    renderWithRouter(<NotFound />);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i });
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.src).toBe(imgUrl);
+  });
+});
